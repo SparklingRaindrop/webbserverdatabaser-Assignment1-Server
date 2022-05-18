87372e394d8eff1cfe77c938b23376cac734da59
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-05-18 13:27:17 +0200

    Delete unneccesary part

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ async function init() {
     
     const server = http.createServer((req, res) => {
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-        res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, DELETE, OPTIONS, POST, PUT");
         res.setHeader("Access-Control-Allow-Headers",
             "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
