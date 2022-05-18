55d5eb2bc47c47e60d455ed0fa859970c6aa8002
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-05-18 10:57:14 +0200

    Add save, write, read json file

--- server.js
+++ server.js
@@ -1,150 +1,150 @@
 const http = require('http');
 const fs = require('fs/promises');
+const { existsSync } = require('fs');
 
 const PORT = 5000;
 const file = './data.json';
 let tasks;
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, DELETE, OPTIONS, POST, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    console.log(`Received request to ${req.url} with method ${req.method}`);
+init();
 
-
-    const directory = req.url.split('/').filter(item => item !== '');
-    // All the request URL has to begin with 'todos'
-    if (directory[0] !== 'todos') {
-        res.statusCode = 404;
-        res.end();
-        return;
-    }
-
-    const method = req.method;
-    const targetId = Number(directory[1]);
-    const targetIndex = tasks.findIndex(task => task.id === targetId);
-    switch (method) {
-        case 'OPTIONS':
-            res.statusCode = 200;
+async function init() {
+    tasks = await read(file);
+    
+    const server = http.createServer((req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, DELETE, OPTIONS, POST, PUT");
+        res.setHeader("Access-Control-Allow-Headers",
+            "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        );
+        console.log(`Received request to ${req.url} with method ${req.method}`);
+    
+        const directory = req.url.split('/').filter(item => item !== '');
+        // All the request URL has to begin with 'todos'
+        if (directory[0] !== 'todos') {
+            res.statusCode = 404;
             res.end();
-        break;
-        case 'GET':
-            if (directory.length === 1) {
-                res.writeHead(200, {
-                    'Content-Type': 'application/json'
-                });
-                res.end(JSON.stringify(tasks));
-            } else if (directory.length === 2) {
-                if (targetIndex > -1) {
+            return;
+        }
+    
+        const method = req.method;
+        const targetId = Number(directory[1]);
+        const targetIndex = tasks.findIndex(task => task.id === targetId);
+        switch (method) {
+            case 'OPTIONS':
+                res.statusCode = 200;
+                res.end();
+            break;
+            case 'GET':
+                if (directory.length === 1) {
                     res.writeHead(200, {
                         'Content-Type': 'application/json'
                     });
-                    res.end(JSON.stringify(tasks[targetIndex]));
+                    res.end(JSON.stringify(tasks));
+                } else if (directory.length === 2) {
+                    if (targetIndex > -1) {
+                        res.writeHead(200, {
+                            'Content-Type': 'application/json'
+                        });
+                        res.end(JSON.stringify(tasks[targetIndex]));
+                    } else {
+                        // There is no data with the provided ID 
+                        res.statusCode = 404;
+                        res.end();
+                    }
+                }
+            break;
+            case 'POST':
+                if (directory.length === 1) {
+                    req.on('data', (chunk) => {
+                        const data = JSON.parse(chunk);
+                        if(isValid(data)) {
+                            const newTask = {};
+                            newTask.taskName = data.taskName;
+                            newTask.id = Number(Date.now().toString() + parseInt(Math.random() * 10000));
+                            newTask.completion = data.completion;
+                            tasks.push(newTask);
+    
+                            save();
+    
+                            res.setHeader('Location', `/todos/${newTask.id}`);
+                            res.statusCode = 201;
+                            res.end();
+                        } else {
+                            // Data is not valid
+                            res.statusCode = 400;
+                            res.end();
+                        }
+                    });
                 } else {
-                    // There is no data with the provided ID 
+                    // Wrong directory or no data with the method "POST"
+                    res.statusCode = 400;
+                    res.end();
+                }
+            break;
+            case 'PUT':
+            case 'PATCH':
+                if(directory.length !== 2) {
+                    // Wrong directory or no data with the method "PUT" & "PATCH"
+                    res.statusCode = 400;
+                    res.end();
+                    return;
+                }
+                if (targetIndex > -1) {
+                    req.on('data', (chunk) => {
+                        const data = JSON.parse(chunk);
+                        if(isValid(data, method)) {
+                            if (method === 'PUT') {
+                                const newData = {
+                                    taskName: data.taskName,
+                                    id: targetId, // TODO: MAKE sure this
+                                    completion: data.completion,
+                                };
+                                tasks[targetIndex] = newData;
+                            } else {
+                                tasks[targetIndex] = {
+                                    ...tasks[targetIndex],
+                                    ...data,
+                                }
+                            }
+    
+                            save();
+    
+                            res.statusCode = 204;
+                            res.end();
+                        } else {
+                            // Data isn't valid
+                            res.statusCode = 400;
+                            res.end();
+                        }
+                    });
+                } else {
+                    // There is no such data with the provided ID 
                     res.statusCode = 404;
                     res.end();
                 }
-            }
-        break;
-        case 'POST':
-            if (directory.length === 1) {
-                req.on('data', (chunk) => {
-                    const data = JSON.parse(chunk);
-                    if(isValid(data)) {
-                        const newTask = {};
-                        newTask.taskName = data.taskName;
-                        newTask.id = Number(Date.now().toString() + Math.random() * 100000);
-                        newTask.completion = data.completion;
-                        tasks.push(newTask);
+            break;
+            case 'DELETE':
+                if (targetIndex > -1) {
+                    tasks.splice(targetIndex, 1);
+    
+                    save();
+    
+                    res.statusCode = 204;
+                    res.end();
+                } else {
+                    // There is no such data with the provided ID 
+                    res.statusCode = 404;
+                    res.end();
+                }
+            break;
+        }
+    });
 
-                        save();
-
-                        res.setHeader('Location', `/todos/${newTask.id}`);
-                        res.statusCode = 201;
-                        res.end();
-                    } else {
-                        // Data is not valid
-                        res.statusCode = 400;
-                        res.end();
-                    }
-                });
-            } else {
-                // Wrong directory or no data with the method "POST"
-                res.statusCode = 400;
-                res.end();
-            }
-        break;
-        case 'PUT':
-        case 'PATCH':
-            if(directory.length !== 2) {
-                // Wrong directory or no data with the method "PUT" & "PATCH"
-                res.statusCode = 400;
-                res.end();
-                return;
-            }
-            if (targetIndex > -1) {
-                req.on('data', (chunk) => {
-                    const data = JSON.parse(chunk);
-                    if(isValid(data, method)) {
-                        if (method === 'PUT') {
-                            const newData = {
-                                taskName: data.taskName,
-                                id: targetId, // TODO: MAKE sure this
-                                completion: data.completion,
-                            };
-                            tasks[targetIndex] = newData;
-                        } else {
-                            tasks[targetIndex] = {
-                                ...tasks[targetIndex],
-                                ...data,
-                            }
-                        }
-
-                        save();
-
-                        res.statusCode = 204;
-                        res.end();
-                    } else {
-                        // Data isn't valid
-                        res.statusCode = 400;
-                        res.end();
-                    }
-                });
-            } else {
-                // There is no such data with the provided ID 
-                res.statusCode = 404;
-                res.end();
-            }
-        break;
-        case 'DELETE':
-            if (targetIndex > -1) {
-                tasks.splice(targetIndex, 1);
-
-                save();
-
-                res.statusCode = 204;
-                res.end();
-            } else {
-                // There is no such data with the provided ID 
-                res.statusCode = 404;
-                res.end();
-            }
-        break;
-    }
-});
-
-init();
-
-server.listen(PORT, () => {
-    console.log(`The server runs on ${PORT}`)
-});
-
-async function init() {
-    const savedData = await read(file);
-    tasks = savedData;
-    console.log(tasks);
+    server.listen(PORT, () => {
+        console.log(`The server runs on ${PORT}`)
+    });
 }
 
 function isValid(data, method = 'PUT') {
@@ -162,7 +162,11 @@ function isValid(data, method = 'PUT') {
   	return hasRequiredProps && hasCorrectTypes && Object.keys(properties).length === keys.length;
 }
 
-async function read(file){
+async function read() {
+    if (!existsSync(file)) {
+        console.log('Creating a new save file.');
+        await fs.writeFile(file, JSON.stringify([]));
+    }
     try {
         const rawData = await fs.readFile(file);
         const result = await JSON.parse(rawData);
@@ -173,7 +177,7 @@ async function read(file){
     }
 }
 
-async function save(){
+async function save() {
     try {
         await fs.writeFile(file, JSON.stringify(tasks));
         console.log('Save completed');
